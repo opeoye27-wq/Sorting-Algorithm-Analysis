c970b28352f5afd22c822ed73d39f38a15e83695
opeoye27-wq <opeoye27@gmail.com>
2026-03-13 08:51:30 -0400

    Fix typo in plot title for sorted performance

--- src/plot_results_sorted.py
+++ src/plot_results_sorted.py
@@ -16,7 +16,7 @@ plt.plot(sizes, selection_times, label = "Selection Sort")
 
 plt.xlabel("Label Size")
 plt.ylabel("Runtime (seconds)")
-plt.title("Sorting Algorithm Peroformance (Sorted)")
+plt.title("Sorting Algorithm Performance (Sorted)")
 
 plt.legend()
 
