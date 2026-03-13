04ae493318465dc0d0b921047b295d6b18c714d4
opeoye27-wq <opeoye27@gmail.com>
2026-03-13 12:45:55 -0400

    Fix typo in plot title for sorting performance

--- plot_results_etf.py
+++ plot_results_etf.py
@@ -16,7 +16,7 @@ plt.plot(sizes, selection_times, label = "Selection Sort")
 
 plt.xlabel("Label Size")
 plt.ylabel("Runtime (seconds)")
-plt.title("Sorting Algorithm Peroformance (End to Front))")
+plt.title("Sorting Algorithm Performance (End to Front))")
 
 plt.legend()
 
